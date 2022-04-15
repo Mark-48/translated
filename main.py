f05f16cdd73124846ffbe1cea38c2bdd04c3bc13
Maksim <maxgubaref2017@yandex.ru>
2022-04-15 03:13:20 +0300

    append cycle FOR and print text

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 def splited_txt(text):
     txt = text.split()
+
     return txt
 
 def splited_words(word):
--- tests/test_main.py
+++ tests/test_main.py
@@ -2,8 +2,10 @@ import pytest
 from main import splited_txt, splited_words
 
 def test_split_text():
+
     assert splited_txt("Hello World !") == ['Hello', 'World', '!']
 
 
 def test_split_words():
-    assert splited_words('Hello') == ['H', 'e', 'l', 'l', 'o' ]
+
+    assert not splited_words('Hello') == ['H', 'e', 'l', 'l', 'o' ]
