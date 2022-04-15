ffe31ff82658e144a191943bfbbdbafc8cc7eb12
Maksim <maxgubaref2017@yandex.ru>
2022-04-15 03:21:14 +0300

    append array rus and eng words

--- main.py
+++ main.py
@@ -10,5 +10,8 @@ def splited_words(word):
 
 source_text = input("text - ")
 
+eng_word = ["`", 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', "'", 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.']
+rus_word = ['`', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю']
+
 for w in splited_txt(source_text):
     print(splited_words(w))
